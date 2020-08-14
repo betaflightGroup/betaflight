5d7eb11a9990fefd83cf7a7132ef179cef045cb2
Steve Evans <steve@stevemacpro.scevans.com>
2020-08-14 16:42:20 +0100

    Added optimised SPI DMA support for F4/F7/H7 access to FLASH for BB and port all drivers to new API Add G4 SPI API support using HAL in polled only mode

--- src/main/vcpf4/usbd_desc.c
+++ src/main/vcpf4/usbd_desc.c
@@ -70,8 +70,8 @@
   #define USBD_PRODUCT_HS_STRING          USBD_PRODUCT_STRING
   #define USBD_PRODUCT_FS_STRING          USBD_PRODUCT_STRING
 #else
-  #define USBD_PRODUCT_HS_STRING          "STM32 Virtual ComPort in HS mode"
-  #define USBD_PRODUCT_FS_STRING          "STM32 Virtual ComPort in FS Mode"
+#define USBD_PRODUCT_HS_STRING          "STM32 Virtual ComPort in HS mode"
+#define USBD_PRODUCT_FS_STRING          "STM32 Virtual ComPort in FS Mode"
 #endif /* USBD_PRODUCT_STRING */
 
 #ifdef USBD_SERIALNUMBER_STRING
@@ -224,8 +224,8 @@ uint8_t *  USBD_USR_DeviceDescriptor( uint8_t speed , uint16_t *length)
     (void)speed;
 #ifdef USE_USB_CDC_HID
     if (usbDevConfig()->type == COMPOSITE) {
-	    *length = sizeof(USBD_DeviceDesc_Composite);
-	    return USBD_DeviceDesc_Composite;
+        *length = sizeof(USBD_DeviceDesc_Composite);
+        return USBD_DeviceDesc_Composite;
     }
 #endif
     *length = sizeof(USBD_DeviceDesc);
